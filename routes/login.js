864efde27ad484c8961eaf5c1ad27078c667e486
Madhu <puppalamadhuvenu9391@gmail.com>
2025-11-26 23:01:58 +0530

    solved cookie issue

--- routes/login.js
+++ routes/login.js
@@ -39,14 +39,14 @@ router.post("/", async (req, res) => {
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 120 * 60 * 1000, // 2hr
     });
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
